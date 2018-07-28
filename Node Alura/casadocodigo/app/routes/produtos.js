31167a264c01a87095f4903b68ceb124ce85bf0c
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2018-07-28 17:47:32 -0300

    Já está gravando no banco

--- Node Alura/casadocodigo/app/routes/produtos.js
+++ Node Alura/casadocodigo/app/routes/produtos.js
@@ -5,7 +5,16 @@ module.exports = function(app) {
         // res.send('listagem produtos');
 
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco(connection);
+        // var produtosBanco = app.infra.ProdutosDAO(connection);
+        // A atribuição acima, sem new, torna o 'this' dentro do objeto global. Sendo assim, caso tenhamos
+        // a situação abaixo, poderemos ter problemas.
+        // EX: uma segunda instancia var produtosBancos2 = app.infra.ProdutosDAO(connection, 'valor 2');
+        // Considerando que produtos banco receba um valor, e dentro de produtos banco tenha this.valor = valor
+        // onde valor é o passado por parâmetro, as duas instancias vão ter o 'valor 2';
+        // Para resolver isso se utiliza o new para receber uma nova instância, conforme abaixo.
+
+
+        var produtosBanco = new app.infra.ProdutosDAO(connection);
 
         produtosBanco.lista(function(err, result) {
                 // console.log(err);
@@ -17,5 +26,26 @@ module.exports = function(app) {
         connection.end();
     
 
+    })
+
+    app.get('/produtos/form', function(req, res) {
+        res.render('produtos/form.ejs');
+    
+    })
+    
+    app.post('/produtos/salva', function(req, res) {
+
+        var produto = req.body;
+
+        console.log(produto);
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.salva(produto, function(erros, resultados) {
+            res.render('produtos/lista');
+        });
+
+      
     })
 };
--- Node Alura/casadocodigo/config/express.js
+++ Node Alura/casadocodigo/config/express.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var load = require('express-load')
+var bodyParser = require('body-parser');
 
 module.exports = function() {
 
     var app = express();
     app.set('view engine', 'ejs').set('views','./app/views');
+
+    app.use(bodyParser.urlencoded({extended: true}));
 
     // tudo que estiver dentro da pasta routes, já vai ser carregado dentr o de app
     // {cwd: 'app'} informa a partir de qual pasta deve procurar a pasta routes e infra. Não é obrigatorio,
