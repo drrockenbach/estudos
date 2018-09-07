b9cdc9a50b6c95c5ca2fa9770eecd45e8a542b70
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2018-09-07 13:32:27 -0300

    Projeto completo

--- React/Udemy/react-complete-guide/src/containers/App.js
+++ React/Udemy/react-complete-guide/src/containers/App.js
@@ -1,111 +1,159 @@
-import React, { Component } from 'react';
-import Classes from './App.css';
-import Persons from '../components/Persons/Persons';
-import Cookpit from '../components/Cookpit/Cookpit';
+import React, { PureComponent } from "react";
 
-class App extends Component {
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+import Aux from "../hoc/Aux";
+import withClass from "../hoc/WithClass";
 
-  state = {
-    persons: [
-      {id: 'person1', name: 'Diomar', age: 32},
-      {id: 'person2', name: 'Rafa', age: 3},
-      {id: 'person3', name: 'Vini', age: 0}
-    ],    
-    username: 'Diomar',
-    showPersons: false
+export const AuthContext = React.createContext(false);
+
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] Inside Constructor", props);
+    this.state = {
+      persons: [
+        { id: "asfa1", name: "Max", age: 28 },
+        { id: "vasdf1", name: "Manu", age: 29 },
+        { id: "asdf11", name: "Stephanie", age: 26 }
+      ],
+      otherState: "some other value",
+      showPersons: false,
+      toggleClicked: 0,
+      authenticated: false
+    };
   }
 
-  // switchNameHandler = (newName) => {
-  //   // console.log("Was clicked!");
-  //   // DON'T DO THAT: this.state.persons[0].name = 'Rafael';
-  //   this.setState({
-  //       persons : [
-  //         {name: newName, age: 32},
-  //         {name: 'Rafael', age: 3}
-  //       ]
-  //     }
-  //   )
+  componentWillMount() {
+    console.log("[App.js] Inside componentWillMount()");
+  }
+
+  componentDidMount() {
+    console.log("[App.js] Inside componentDidMount()");
+  }
+
+  // shouldComponentUpdate ( nextProps, nextState ) {
+  //   console.log( '[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState );
+  //   return nextState.persons !== this.state.persons ||
+  //     nextState.showPersons !== this.state.showPersons;
   // }
 
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-
-    // Essa é a forma mais nova de criar um novo array sem fazer referencia para o antigo, criando uma copia. 
-    // O trecho acima, com slice, também faz isso.
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside componentWillUpdate",
+      nextProps,
+      nextState
+    );
   }
-  
-  nameChangedHandler = (event, id) => {
-    // console.log("Was clicked!");
-    // DON'T DO THAT: this.state.persons[0].name = 'Rafael';
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log(
+      "[UPDATE App.js] Inside getDerivedStateFromProps",
+      nextProps,
+      prevState
+    );
+
+    return prevState;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log(
+      "[UPDATE App.js] Inside getSnapshotBeforeUpdate"
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("[UPDATE App.js] Inside componentDidUpdate");
+  }
+
+  // state = {
+  //   persons: [
+  //     { id: 'asfa1', name: 'Max', age: 28 },
+  //     { id: 'vasdf1', name: 'Manu', age: 29 },
+  //     { id: 'asdf11', name: 'Stephanie', age: 26 }
+  //   ],
+  //   otherState: 'some other value',
+  //   showPersons: false
+  // }
+
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
 
-    const person = {...this.state.persons[personIndex]};
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
     person.name = event.target.value;
 
     const persons = [...this.state.persons];
-
     persons[personIndex] = person;
 
-    this.setState({
-        persons : persons
-      }
-    )
-  }
+    this.setState({ persons: persons });
+  };
 
-  switchUserNameHandler = (event) => {
-    this.setState(
-      {
-        username: event.target.value
-      }
-    )
-  }
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
   togglePersonsHandler = () => {
-    const doShow = this.state.showPersons;
-    this.setState({showPersons: !doShow});
-  }
+    const doesShow = this.state.showPersons;
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
+    });
+  };
+
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
 
   render() {
-
+    console.log("[App.js] Inside render()");
     let persons = null;
-    
+
     if (this.state.showPersons) {
       persons = (
-        <div>
-          <Persons persons={this.state.persons} clicked={this.deletePersonHandler}  changed={this.nameChangedHandler}/>
-        </div>
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
       );
     }
 
-    const assignedClasses = [];
-
-    if (this.state.persons.length <=2) {
-      assignedClasses.push(Classes.red); 
-    }
-
-    if (this.state.persons.length <=1) {
-      assignedClasses.push(Classes.bold); 
-    }
-
     return (
-      <div className={Classes.App}>
-        <Cookpit showPersons={this.state.showPersons}
+      <Aux>
+        <button
+          onClick={() => {
+            this.setState({ showPersons: true });
+          }}
+        >
+          Show Persons
+        </button>
+        <Cockpit
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
           persons={this.state.persons}
+          login={this.loginHandler}
           clicked={this.togglePersonsHandler}
-          appTitle={this.props.title}/>
-        {persons}
-      </div>
+        />
+        <AuthContext.Provider value={this.state.authenticated}>
+          {persons}
+        </AuthContext.Provider>
+      </Aux>
     );
-    // No final das contas, isso abaixo é o que o react vai fazer com o código acima. Apesar de acima parecer HTML, lembrando que é JSX
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
