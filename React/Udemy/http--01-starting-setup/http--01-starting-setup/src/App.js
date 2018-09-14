1e9defd742fb8d9942c65557f90dd96a95d99295
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2018-09-14 17:04:50 -0300

    Até aula 180

--- React/Udemy/http--01-starting-setup/http--01-starting-setup/src/App.js
+++ React/Udemy/http--01-starting-setup/http--01-starting-setup/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import Blog from './containers/Blog/Blog';
 
+import { BrowserRouter} from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
