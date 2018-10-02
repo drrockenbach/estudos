37a22ecd8a32c6c29696d36d1635c1cf315d884c
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2018-10-02 09:05:17 -0300

    Removendo UdemyWorkSpace.code-workspace

---
+++ React/Udemy/lote.sh
@@ -0,0 +1,38 @@
+cd burger-builder
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd http--01-starting-setup
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd react-complete-guide
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd redux--01-start
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd redux--assignment-2-problem
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd routing--assignment-solution
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+cd tarefa2-item58
+rm -rf node_modules
+git add .
+git commit -m 'Removendo node_modules'
+cd ..
+rm UdemyWorkSpace.code-workspace
+git add .
+git commit -m 'Removendo UdemyWorkSpace.code-workspace'
