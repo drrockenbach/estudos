f87ad19893037747713da285113b46ec37b56edd
Diomar Rodrigo Rockenbach <diomar.rockenbach@meta.com.br>
2018-10-05 11:00:58 -0300

    Últimas alterações burger builder e projeto criado do zero para entender o webpack

---
+++ React/Udemy/WebPack/src/container/Users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import PizzaImage from '../components/PizzaImagem/PizzaImage';
+
+export default class Users extends Component {
+  render() {
+    return (
+      <div>
+          <h1>The Users</h1>
+          <p>Awesome users on board of this course!</p>
+      </div>
+    );
+  };
+};
